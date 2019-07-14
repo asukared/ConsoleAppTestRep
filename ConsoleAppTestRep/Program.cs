6927a63c2f86460e1ea43dfd2e1740c845e21831
asukared <kdum@tut.by>
2019-07-15 00:20:34 +0300

    Add console write git update

--- ConsoleAppTestRep/Program.cs
+++ ConsoleAppTestRep/Program.cs
@@ -10,6 +10,7 @@ namespace ConsoleAppTestRep
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Git update1");
             Console.WriteLine("ConsoleAppTestRep");
             Console.ReadLine();
         }
